20dfb97970445d084ecc5dd42dd14ba1932f2a29
WENJIE HUANG <JankinHuang0302@163.com>
2024-07-21 21:28:11 +0800

    [Anne Engine] Add Floder Structure

---
+++ Editor/Editor.h
@@ -0,0 +1,12 @@
+#pragma once
+// Engine
+
+// Editor
+
+namespace AnneEngine
+{
+    namespace Application
+    {
+        
+    }
+}
---
+++ Editor/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Editor.h"
+int main(int argc, char **argv)
+{
+    using namespace AnneEngine;
+    return 0;
+}
