037f7ad1cc3ca40cd309a34e7954083e6695a194
WENJIE HUANG <JankinHuang0302@163.com>
2024-07-23 08:47:24 +0800

    [Anne Engine] 增加单例模式的实现

--- Engine/Core/Singleton.hpp
+++ Engine/Core/Singleton.hpp
@@ -6,7 +6,31 @@
 #include <vector>
 #include <unordered_map>
 #include <functional>
-// #include <glm.hpp>
-// #include <gtc/type_ptr.hpp>
+#include <glm.hpp>
+#include <gtc/type_ptr.hpp>
 
-// namespace 
+namespace AnneEngine
+{
+    template <typename T>
+    class Singleton
+    {
+    public:
+        static T &Instance();  // 通过Instance 访问全局唯一实例
+
+        Singleton(const Singleton &) = delete;  // 删除拷贝构造
+        Singleton &operator=(const Singleton) = delete;   // 删除拷贝赋值
+
+    protected:
+        struct token
+        {
+        };
+        Singleton(){} // Singleton的构造 + 构造的入参token 放在protected内
+    };
+
+    template <typename T>
+    T &Singleton<T>::Instance()
+    {
+        static std::unique_ptr<T> _instance{new T{token{}}};  // unique_ptr 创建单例
+        return *_instance;
+    }
+}
