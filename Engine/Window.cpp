905745fdd54c5b13887a448ace1188916440e819
WENJIE HUANG <JankinHuang0302@163.com>
2024-07-24 22:30:19 +0800

    [Anne Engine] 增加Window.h的头文件内容

---
+++ Engine/Window.cpp
@@ -0,0 +1,3 @@
+#include "Engine/Window.h"
+#include <iostream>
+using namespace AnneEngine;
--- Engine/Window.h
+++ Engine/Window.h
@@ -7,7 +7,7 @@ namespace AnneEngine
 {
     namespace io
     {
-        // 窗口回调函数的签名
+        // 一些functor的别名
         using onResetFunc = std::function<void()>;  // 重置
         using onKeyFunc = std::function<void(int, int, int, int)>; // 键盘按压
         using onMouseButtonFunc = std::function<void(int, int, int)>; // 鼠标点击
@@ -20,7 +20,40 @@ namespace AnneEngine
     // 合适需要在类的内部成员函数中创建一个指向自身的 std::shared_ptr的场景
     class EngineWindow : public std::enable_shared_from_this<EngineWindow>
     {
+    
+    public:
+        struct Size
+        {
+            unsigned int width, height;
+        }; 
+    
+    public:
+        // 构造 析构
+        EngineWindow() = delete;
+        EngineWindow(EngineWindow::Size size, std::string_view title, int32_t major_version, int32_t minor_version);
+        ~EngineWindow();
 
+    public:
+        // windows
+        void Resize(unsigned int width, unsigned int height);
+        void RescaleViewport();
+        void SwapBuffers();
+        void PollEvents();
+        bool ShouldClose();
+        void Terminate();
+
+        // IO callback
+        void RegisterIO();
+        void RegisterOnResetFunc(io::onResetFunc func);
+        void RegisterOnKeyFunc(io::onKeyFunc func);
+        void RegisterOnMouseButtonFunc(io::onMouseButtonFunc func);
+        void RegisterOnCursorPosFunc(io::onCursorPosFunc func);
+        void RegisterOnReSizeFunc(io::onWindowSizeFunc func);
+        void RegisterOnScrollFunc(io::onScrollFunc func);  
+
+    protected:
+        GLFWwindow *m_window{nullptr};
+        EngineWindow::Size m_size;
     };
 }
 
